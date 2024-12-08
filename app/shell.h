a596266521d494ff94cb0ce65b430b762ffeb1f8
Muhammad Arief Fatkhurrahman <m.fatkhurahman@salt.co.id>
2024-12-08 22:07:02 +0700

    implement execute from executable

--- app/shell.h
+++ app/shell.h
@@ -1,7 +1,7 @@
 #ifndef __SHELL_H__
 
 #define MAX_INPUT_BUFFER 100
-#define MAX_CMD_LENGTH 10
+#define MAX_CMD_LENGTH 100
 #define EXIT_0 0xFF
 #define ERR_INVALID_CMD 0x01
 #define ERR_TOKEN_LENGTH_LIMIT 0x02
